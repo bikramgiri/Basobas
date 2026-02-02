58fa5f7082963018bf3e13cbb3f0693cb214b220
munal <emailaddress9810@gmail.com>
2026-02-02 11:54:50 +0545

    Added forget password page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Register from './pages/auth/Register'
 import Home from './pages/home/Home'
 import Login from './pages/auth/Login'
+import ForgotPassword from './pages/auth/ForgetPassword'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/forgetpassword" element={<ForgotPassword />} />
       </Routes>
     </BrowserRouter>
   )
